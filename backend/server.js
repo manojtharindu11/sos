7268a18139996b8610d5e9fb3a86ddb2cd640b2e
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-04-24 22:19:59 +0530

    chore: update environment variables and comment out Socket.IO setup

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ const server = http.createServer(app);
 const routes = require("./routes/routes");
 
 // Socket.IO setup
-const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
-});
+// const io = new Server(server, {
+//   cors: { origin: process.env("FRONTEND_URL"), methods: ["GET", "POST"] },
+// });
 
 // MongoDB Connection
 mongoose
@@ -22,10 +22,10 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1', routes); // API routes
+app.use("/api/v1", routes); // API routes
 
 // Socket logic
-require("./socket/socket")(io);
+// require("./socket/socket")(io);
 
 // Start server
 const PORT = process.env.PORT || 5000;
