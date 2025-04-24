c633299c791c35fe3aa1f23faa431d897978d464
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-04-24 22:04:13 +0530

    feat: update user schema to correct username field and ensure email is required

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  _id: String,
-  userName: {
+  username: {
     type: String,
     required: true,
     unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
   },
   password: {
     type: String,
