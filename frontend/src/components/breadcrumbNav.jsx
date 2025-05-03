cc712d62ea881ac926078a4f94be05565f4276ed
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-05-03 20:59:40 +0530

    feat: add BreadcrumbNav component for improved navigation

---
+++ frontend/src/components/breadcrumbNav.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const BreadcrumbNav = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const pathnames = location.pathname.split("/").filter((x) => x);
+
+  useEffect(() => {
+    console.log(pathnames);
+  }, []);
+
+  const handleBack = () => {
+    navigate(-1); // navigate to previous page
+  };
+
+  const generatePath = (index) => {
+    return "/" + pathnames.slice(0, index + 1).join("/");
+  };
+
+  const breadCrumpItem = (index, path, label) => {
+    return (
+      <BreadcrumbItem key={index}>
+        <BreadcrumbLink onClick={() => navigate(path)}>{label}</BreadcrumbLink>
+      </BreadcrumbItem>
+    );
+  };
+
+  const generateBreadCrumpItems = pathnames.map((name, index) => {
+    const path = generatePath(index);
+    const label = name.charAt(0).toLowerCase() + name.slice(1);
+    return breadCrumpItem(index, path, label);
+  });
+
+  return (
+    <Breadcrumb mb={4}>
+      <BreadcrumbItem>
+        <BreadcrumbLink onClick={handleBack}>..</BreadcrumbLink>
+      </BreadcrumbItem>
+
+      {generateBreadCrumpItems}
+    </Breadcrumb>
+  );
+};
+
+export default BreadcrumbNav;
