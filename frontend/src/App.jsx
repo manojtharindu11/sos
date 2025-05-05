8edee8fe51d60eea43039db937f9b87170350f06
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-05-05 23:24:48 +0530

    feat: implement mobile view support with MobileBlocker component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { UserProvider } from "./context/userContext";
 import AppRoutes from "./routes/AppRoutes";
+import MobileBlocker from "./components/mobileBlocker";
+import { useMediaQuery } from "@chakra-ui/react";
 
 function App() {
+  const [isMobile] = useMediaQuery("(max-width: 1100px)");
+
+  const displayContent = isMobile ? <MobileBlocker /> : <AppRoutes />;
+
   return (
     <ChakraProvider>
-      <UserProvider>
-        <AppRoutes/>
-      </UserProvider>
+      <UserProvider>{displayContent}</UserProvider>
     </ChakraProvider>
   );
 }
