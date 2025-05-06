cab45612a615ce9547af5cf629b4f00ceccb03b2
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-05-06 15:47:27 +0530

    fix: specify host in server.listen for better accessibility

--- backend/server.js
+++ backend/server.js
@@ -29,4 +29,6 @@ require("./socket/socket")(io);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
