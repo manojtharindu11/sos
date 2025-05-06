d12b41dec8c9bab5bbac68dbfbfcc65a6ad302ca
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-05-06 10:39:59 +0530

    feat: add route to get current game score with authentication

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const authController = require("../controller/authController");
+const gameController = require("../controller/gameController");
 const {
   validateSignup,
   validateLogin,
@@ -10,5 +11,10 @@ router.post("/auth/login", validateLogin, authController.login);
 router.post("/auth/signup", validateSignup, authController.signup);
 router.post("/auth/refresh", authController.refreshToken);
 router.get("/auth/protected", authMiddleware, authController.protected);
+router.get(
+  "/game/get-current-score",
+  authMiddleware,
+  gameController.getCurrentScore
+);
 
 module.exports = router;
