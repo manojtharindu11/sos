72b97f45bd3d37669b197d13a2f64f4d56639819
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-05-06 14:04:19 +0530

    fix: correct import path for ActivePlayers component

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -19,7 +19,7 @@ import ProfileCard from "../components/ProfileCard";
 import Timer from "../components/Timer";
 import GameOver from "../components/GameOver";
 import BreadcrumbNav from "../components/breadcrumbNav";
-import ActivePlayers from "../components/ActivePlayers";
+import ActivePlayers from "../components/activePlayers";
 import StartGameButton from "../components/StartGameButton";
 
 function Game() {
